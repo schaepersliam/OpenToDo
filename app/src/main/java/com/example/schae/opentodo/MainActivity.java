35a7b9b6591d2132718d2f29e4abd3ac69247596
Liam Schäpers <schaepersliam@gmail.com>
2018-06-19 15:01:33 +0200

    Created a overall new design for the whole app!

--- app/src/main/java/com/example/schae/opentodo/MainActivity.java
+++ app/src/main/java/com/example/schae/opentodo/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.schae.opentodo;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.NumberPicker;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    String input_text = "";
+    Dialog AddDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,23 +32,27 @@ public class MainActivity extends AppCompatActivity {
         final ArrayList<Entry> entries = new ArrayList<>();
         final EntryAdapter adapter = new EntryAdapter(this, entries);
         final ListView list_view = findViewById(R.id.list_view);
-        final EditText input = findViewById(R.id.todo_input_edittext);
         final Button add = findViewById(R.id.todo_add_button);
-        final Button remove = findViewById(R.id.remove_button);
+        final ImageButton remove = findViewById(R.id.remove_button);
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String input_text = input.getText().toString();
-                if (input_text.isEmpty()) {
-                    Toast toast = Toast.makeText(MainActivity.this,"Your ToDo is empty!",Toast.LENGTH_SHORT);
-                    toast.show();
-                } else {
-                    entries.add(new Entry(input_text));
-                    adapter.notifyDataSetChanged();
-                    input.setText("");
-                    Log.e("Input: ", input_text);
-                }
+                AddDialog = new Dialog(MainActivity.this);
+                AddDialog.setContentView(R.layout.custom_alertdialog_add_todo);
+                AddDialog.setTitle("Add new ToDo");
+                Button add = AddDialog.findViewById(R.id.dialog_add_todo);
+                final EditText input_todo = AddDialog.findViewById(R.id.todo_input_edittext);
+                add.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        entries.add(new Entry(input_todo.getText().toString()));
+                        adapter.notifyDataSetChanged();
+                        AddDialog.dismiss();
+                    }
+                });
+                AddDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+                AddDialog.show();
             }
         });
 
